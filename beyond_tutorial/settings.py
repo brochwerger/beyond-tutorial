50c2e0ab23ce1f8fbd2c4baf48988e6ab60928ab
Benny Rochwerger <brochwer@redhat.com>
2022-11-07 19:22:35 +0200

    Switched from sqllite to postgress

--- beyond_tutorial/settings.py
+++ beyond_tutorial/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'beyond_tutorial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'messages',
+        'USER': 'beyond',
+        'PASSWORD': 'beyond',
+        'HOST': 'dbserver',
+        'PORT': '5432',
     }
 }
 
